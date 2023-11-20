b5f07001ae67dbcce35eabb241cb0e028afa1c05
feijaozinho17327 <Kira_belmontt@protonmail.com>
2023-11-20 07:15:17 -0300

    atualizacao do codigo

---
+++ README.md
@@ -0,0 +1,6 @@
+## Chat em Java
+
+Um pequeno projeto de aplicação de sockets e threads em java seguindo as seguintes video aulas.
+
+https://www.youtube.com/watch?v=MtAfYUW7fJ4
+https://www.youtube.com/watch?v=KPASiXiD9zQ
--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,2 +1,68 @@
-public class ChatClient {
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient implements Runnable{
+    private final String SERVER_ADDRESS = "127.0.0.1";
+    private ClientSocket clientSocket;
+
+    private Scanner scan;
+
+
+    public ChatClient() {
+        scan = new Scanner(System.in);
+
+
+
+
+    }
+    public void start() throws IOException {
+
+
+        try {
+            clientSocket = new ClientSocket(new Socket(SERVER_ADDRESS, 5000));
+            new Thread(this).start();
+            messageLoop();
+        } finally {
+            clientSocket.close();
+        }
+
+    }
+    @Override
+    public void run() {
+        String msg;
+
+        while ((msg = clientSocket.getMessage()) != null) {
+            System.out.println("Mensagem recebida: " + msg);
+        }
+    }
+    public void messageLoop() throws IOException {
+        String msg;
+
+        do {
+            System.out.print("digite uma mensagem: ");
+            msg = scan.nextLine();
+            clientSocket.sendMsg(msg);
+
+
+        }while (!msg.equals("--sair"));
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        try {
+            ChatClient chatClient = new ChatClient();
+            chatClient.start();
+        } catch (IOException e) {
+            System.out.println("Erro ao iniciar cliente " + e.getMessage());
+        }
+
+    }
+
+
 }
